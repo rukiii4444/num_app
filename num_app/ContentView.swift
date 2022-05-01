5b441adffc9c3ddc5c8a3e3897c3eceef8fb9261
rukiii4444 <haruki14rezer@gmail.com>
2022-05-01 20:21:50 +0900

    Initial Commit

---
+++ num_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  num_app
+//
+//  Created by rukiii 4444 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ num_app/num_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  num_appApp.swift
+//  num_app
+//
+//  Created by rukiii 4444 on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct num_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
